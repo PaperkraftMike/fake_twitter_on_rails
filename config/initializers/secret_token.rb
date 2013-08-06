5f8e6dd9ca8d031d15b58274821a9ca3a4010ca1
Michael Cahill <miccahill@gmail.com>
2013-08-06 19:44:55 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FakeTwitterOnRails::Application.config.secret_key_base = '4fdd11ee566714be623a7f8e19a0c2321c570fd4e64cc4260b1f4eca8219dc0b3b1084a3bfb32313a2750b258401b74c6034147ab0ae0179b606d60cd8e3644e'
---
+++ db/migrate/20130806233904_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :city
+      t.string :state
+      t.string :password_hash
+      t.string :password_salt
+      t.string :user_name
+      t.text :bio
+
+      t.timestamps
+    end
+  end
+end
