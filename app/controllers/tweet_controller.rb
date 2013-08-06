84f4cda2feaf54a9817ed2e0f0579e37d66a4c18
Michael Cahill <miccahill@gmail.com>
2013-08-06 19:54:18 -0400

    creating tweets model

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,2 +1,28 @@
 class TweetController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def destroy
+
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).require(:text, :user_id)
+  end
 end
