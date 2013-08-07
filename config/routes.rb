89e07a443029894fb5c91ca764813244bca20a01
Michael Cahill <miccahill@gmail.com>
2013-08-06 21:12:47 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 FakeTwitterOnRails::Application.routes.draw do
   root :to => "home#index"
-<<<<<<< HEAD
-  resources :user
-
-  get "sign_up" => "user#new", :as => "sign_up"
-  resources :users
-=======
   resources :user do 
     resources :tweets 
   end
->>>>>>> master
 end
